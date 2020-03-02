3c934e8922cdfcd2404067b44598b52f80a9cc09
Max R <maxx.ratto@gmail.com>
2020-03-02 12:58:58 -0700

    Added LandingPageContent component, copy, and CSS.

--- src/LandingPageContent/LandingPageContent.js
+++ src/LandingPageContent/LandingPageContent.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import './LandingPageContent.css'
 
 class LandingPageContent extends Component {
     render() {
         return (
-            <section>
-
-            </section>
+            <>
+                <section class="header">
+                    <h1>Simple, swift, film production management.</h1>
+                    <button>Sign up</button>
+                    <button>Login</button>
+                </section>
+                <section class="feature">
+                    <h2>Script breakdown</h2>
+                    <p>Breeze through your script breakdown process with ProductionWeaver's simple, mobile-friendly interface.</p>
+                </section>
+                <section class="feature">
+                    <h2>Elements</h2>
+                    <p>Every element in your productions needs fleshing out, use Elements to keep your thoughts, notes, and ideas in order.</p>
+                </section>
+                <section class="feature">
+                    <h2>We're just getting started. Coming soon:</h2>
+                    <p>Budgets<br />
+                        Shooting schedules<br />
+                        Shot lists<br />
+                        Storyboards<br />
+                        Call sheets<br />
+                        Contact management<br />
+                    </p>
+                </section>
+                <section class="call-to-action">
+                    <h2>Start using ProductionWeaver!</h2>
+                    <button>Get started</button>
+                </section>
+            </>
         )
     }
 }
