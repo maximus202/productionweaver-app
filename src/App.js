4118098e0024fc4417eccd467ba1ae2470976d3a
Max R <maxx.ratto@gmail.com>
2020-03-02 13:04:49 -0700

    Added Footer component, copyright copy, and CSS.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar/NavBar.js';
 import LandingPageContent from './LandingPageContent/LandingPageContent.js';
+import Footer from './Footer/Footer.js';
 
 function App() {
   return (
     <main className='App'>
       <NavBar />
       <LandingPageContent />
+      <Footer />
     </main>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>©2020 ProductionWeaver.com</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
