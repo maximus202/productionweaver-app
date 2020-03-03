32abc365fb1f724639ee6fc970154e23332028f5
Max R <maxx.ratto@gmail.com>
2020-03-02 20:42:29 -0700

    Added smoke testing for Footer, LandingPageContent, Logo, Menu, and NavBar components.

---
+++ src/Menu/Menu.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './Menu';
+
+describe('Menu', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Menu />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
---
+++ src/NavBar/NavBar.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './NavBar.js';
+
+describe('NavBar', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<NavBar />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+});
