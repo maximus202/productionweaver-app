e859907eefa7b25f85be4a7d8296d9f502e572d8
Max R <maxx.ratto@gmail.com>
2020-03-29 14:14:33 -0700

    Uploading official logo.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 class Logo extends Component {
     render() {
         return (
-            <Link to={`/`}><img src="https://pngimage.net/wp-content/uploads/2018/06/logo-placeholder-png.png" alt="ProductionWeaver logo." className="logo" /></Link>
+            <Link to={`/`}><img src="" alt="ProductionWeaver logo." className="logo" /></Link>
         )
     }
 }
