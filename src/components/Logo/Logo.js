8aafb3583f258cf2f68e32416747074618d9f2d5
Max R <maxx.ratto@gmail.com>
2020-03-29 14:41:41 -0700

    Adjusted css for logo and navbar, uploading heroimage.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 class Logo extends Component {
     render() {
         return (
-            <Link to={`/`}><img src="" alt="ProductionWeaver logo." className="logo" /></Link>
+            <Link to={`/`}><img src="https://github.com/maximus202/productionweaver-app/blob/styling/public/PWlogocolor.png?raw=true" alt="ProductionWeaver logo." className="logo" /></Link>
         )
     }
 }
