a3771ca3216352206efdc29eb724626d17a83730
Max R <maxx.ratto@gmail.com>
2020-03-30 18:38:48 -0700

    Removing env object in config.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    env: {
-        API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000',
-        TOKEN_KEY: 'client_auth_token',
-    }
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000',
+    TOKEN_KEY: 'client_auth_token',
 }
