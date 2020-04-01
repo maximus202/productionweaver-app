7c23ab385ebed5905b57c95a1224e97e5e4e5d04
Max R <maxx.ratto@gmail.com>
2020-04-01 05:45:56 -0700

    Implemented most branding elements.

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -12,31 +12,35 @@ class LandingPage extends Component {
                     <section>
                         <h1>Simple, swift, film production management.</h1>
                         <Link to="/sign-up">Sign Up</Link>
-                        <Link to="/login">Login here</Link>
+                        <Link to="/login">Login</Link>
                     </section>
                 </header>
                 <main>
                     <section className="feature">
-                        <h2>Script breakdown</h2>
-                        <p>Breeze through your script breakdown process with ProductionWeaver's simple, mobile-friendly interface.</p>
-                    </section>
-                    <section className="feature">
-                        <h2>Elements</h2>
-                        <p>Every element in your productions needs fleshing out, use Elements to keep your thoughts, notes, and ideas in order.</p>
-                    </section>
-                    <section className="feature">
-                        <h2>We're just getting started. Coming soon:</h2>
-                        <p>Budgets<br />
-                            Shooting schedules<br />
-                            Shot lists<br />
-                            Storyboards<br />
-                            Call sheets<br />
-                            Contact management<br />
+                        <h2>Film production tools finally geared towards indie filmmakers.</h2>
+                        <p>
+                            Running your production on spreadsheets is a great solution... said no one ever. ProductionWeaver's mission is simple.
+                            Provide quality production tools to help you run your productions without spending Hollywood money.
+                        </p>
+                        <p>
+                            ProductionWeaver was designed out of the simple idea that independent filmmakers should be able to have access to production
+                            tools that don't cause dents into their already minimal budgets.
+                        </p>
+                        <p>
+                            During these early stages of development, you can
+                            use ProductionWeaver for free and try out the tools as they're released.
+                        </p>
+                        <p>
+                            The script breakdown tool is the very first tool to become available in ProductionWeaver! Breeze through
+                            your script breakdown process with this simple, mobile-friendly interface. Try it for free today!
+                        </p>
+                        <p>
+                            Tools coming soon: budgeting, shoot scheduling, shot listing, storyboarding, call sheets, contact management and more!
                         </p>
                     </section>
                     <div className="call-to-action">
                         <section>
-                            <h2>Start using Production Weaver!</h2>
+                            <h2>Start using ProductionWeaver!</h2>
                             <Link to="/sign-up">Get started</Link>
                         </section>
                     </div>
