262a055471b56bb6d8bb2a53da76cf912d7ea50a
Max R <maxx.ratto@gmail.com>
2020-07-25 15:26:31 -0600

    Updated logo.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './Logo.css';
 import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
     <>
       <h1>
-        <Link to="/"><img src="https://github.com/maximus202/productionweaver-app/blob/styling/public/PWlogocolor.png?raw=true" alt="ProductionWeaver. Simple, swift, film production management for indie filmmakers." className="logo" /></Link>
+        <Link to="/"><img src="https://github.com/maximus202/productionweaver-app/blob/branding-update/public/pwlogo40h.png?raw=true" alt="ProductionWeaver. Simple, swift, film production management for indie filmmakers." className="logo" /></Link>
       </h1>
     </>
   );
