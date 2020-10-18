181933df17a26a81282f7fffd51c11c0e294af3e
Max R <maxx.ratto@gmail.com>
2020-10-17 22:59:59 -0600

    Cleaned up Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 
-const layout = ( props ) => (
-    <Aux>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+const layout = (props) => (
+  <Aux>
+    <main className={classes.Content}>
+      {props.children}
+    </main>
+  </Aux>
 );
 
 export default layout;
